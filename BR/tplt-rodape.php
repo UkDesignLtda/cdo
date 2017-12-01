ea34b8cdc57d807e5d2e5db894c4591e6b7cd6d8
Uk Design IT User <30817468+ukdesignTI@users.noreply.github.com>
2017-12-01 18:17:15 -0200

    emersonajr mudanças para bootstrap 4
    Mudanças necessárias para o uso do bootstrap 4. Também converti arquivos html em php para fazer uso de partes repetidas (cabeçalho e rodapé). TODO: ajustar largura interna para bater com layout da Alice. Usar páginas oadvogado.php e index.php dentro de BR como modelos.

---
+++ BR/tplt-rodape.php
@@ -0,0 +1,23 @@
+        <footer> 
+            <nav class="nav-horizontal">
+                <ul class="nav-mapa-site list-unstyled">
+                    <li class=""><a href="o-escritorio.html">O ESCRITÓRIO</a></li>
+                    <li class=""><a href="tradicao.htmp">TRADIÇÃO</a></li>
+                    <li class=""><a href="area-de-atuacao.html">ÁREA DE ATUAÇÃO</a></li>
+                    <li class=""><a href="advogados.html">ADVOGADOS</a></li>
+                    <li class=""><a href="noticias.html">NOTÍCIAS</a></li>
+                    <li class=""><a href="carreira.html">CARREIRA</a></li>
+                    <li class=""><a href="contato.html">CONTATO</a></li>
+                </ul>
+            </nav>
+            <div class="copyrights">
+                <p class="text-center">copyrights ©2017 - Cândido de Oliveira Advogados - <a href="http://www.ukdesign.com.br" target="_blank">uk design ltda.</a></p>
+            </div>
+        </footer>
+        <!-- JS train on the go -->
+        <script src="../scripts/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src='../scripts/popper.js' type="text/javascript"></script>
+        <script src="../scripts/bootstrap.min.js" type="text/javascript"></script>
+        <script src="../scripts/jquery.slides.min.js" type="text/javascript"></script>
+
+<?php
